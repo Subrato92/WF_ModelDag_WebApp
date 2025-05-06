9a58b8d39ecaa317c84481d3e6936c7cca0b1012
SUBRATO MONDAL <subrato.mondal18@gmail.com>
2025-05-06 10:57:28 +0530

    TestSuites and TestCodeConfigs added

--- src/components/configuration/TestCodeConfig.jsx
+++ src/components/configuration/TestCodeConfig.jsx
@@ -1,5 +1,7 @@
 "use client"
+import { type } from 'os';
 import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function TestCodeConfig({section}){
     return (
@@ -12,8 +14,24 @@ export default function TestCodeConfig({section}){
 }
 
 export function Parameters(){
+
+    var params = [
+        { name: 'handle_missing_values', type: 'text' },
+        { name: 'max_outlier', type: 'text' },
+        { name: 'num_decimals', type: 'text' },
+        { name: 'max_cats', type: 'text' },
+        { name: 'max_miss_pct', type: 'text' },
+        { name: 'min_cv', type: 'text' },
+        { name: 'p_val', type: 'text' },
+        { name: 'predictors', type: 'text' },
+        { name: 'correction', type: 'text' },
+        { name: 'outlier_datasets', type: 'text' },
+        { name: 'drift_datasets', type: 'text' },
+        { name: 'special_missing_codes', type: 'text' }
+    ]
+
     return (
-        <div style={{display: 'flex', flexDirection: 'column', fontSize: '12px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', fontSize: '12px', height: '77%', overflow: 'scroll'}}>
             <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
                 Select Signed-off Test Code
                 <select name="page_size" id="page_size" style={{border: '1px solid black', borderRadius: '5px', margin: '4px 0px', padding: '8px 12px'}}>
@@ -23,6 +41,28 @@ export function Parameters(){
                     <option value="11216">Code 11216</option>
                     <option value="11217">Code 11217</option>
                 </select>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
+                Fill in the parameters
+                <div style={{display: 'grid', gridTemplateColumns: 'auto auto', gap: '4px', margin: '4px 0px'}}>
+                    {params.map((param, idx) => <input key={idx} placeholder={param.name} type={param.type} style={{ padding: '4px', border: '1px solid grey', borderRadius: '4px'}}/>)}
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
+                Criterion
+                <div style={{display: 'flex', flexDirection: 'row', margin: '4px 0px'}}>
+                    <input placeholder='Criteria Name' type='text' style={{ padding: '4px', border: '1px solid grey', borderRadius: '4px', margin: '4px', marginLeft: '0px'}}/>
+                    <select name="operator" id="operator" style={{border: '1px solid black', borderRadius: '5px', margin: '4px', padding: '8px 12px'}}>
+                        <option value="11213"> {'<'} </option>
+                        <option value="11214"> {'>'} </option>
+                        <option value="11215"> {'='} </option>
+                    </select>
+                    <input placeholder='Criteria Value' type='text' style={{ padding: '4px', border: '1px solid grey', borderRadius: '4px', margin: '4px'}}/>
+                </div>
+                <button style={{border: '1px solid grey', borderRadius: '4px', padding: '4px', width: '175px', justifyContent: 'center', alignItems: 'baseline'}}> 
+                    <AddIcon style={{ fontSize: '14px'}}/>
+                    Add New Criteria
+                </button>
             </div>
         </div>
     )
--- src/components/configuration/TestSuiteConfig.jsx
+++ src/components/configuration/TestSuiteConfig.jsx
@@ -1,5 +1,6 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function TestSuiteConfig({section}){
     return (
@@ -12,8 +13,52 @@ export default function TestSuiteConfig({section}){
 }
 
 export function Parameters(){
+    const [currentTestCode, setCurrentTestCode] = useState('Test Code 11231');
+
+    const testCodes = {
+        'Test Code 11231': {
+            params: [
+                { name: 'handle_missing_values', type: 'text' },
+                { name: 'max_outlier', type: 'text' },
+                { name: 'num_decimals', type: 'text' },
+                { name: 'max_cats', type: 'text' },
+                { name: 'max_miss_pct', type: 'text' },
+                { name: 'min_cv', type: 'text' },
+                { name: 'p_val', type: 'text' },
+                { name: 'predictors', type: 'text' },
+                { name: 'correction', type: 'text' },
+                { name: 'outlier_datasets', type: 'text' },
+                { name: 'drift_datasets', type: 'text' },
+                { name: 'special_missing_codes', type: 'text' }
+            ]
+        },
+        'Test Code 11232': {
+            params: [
+                { name: 'handle_missing_values', type: 'text' },
+                { name: 'max_outlier', type: 'text' },
+                { name: 'num_decimals', type: 'text' },
+                { name: 'max_cats', type: 'text' },
+                { name: 'max_miss_pct', type: 'text' },
+                { name: 'correction', type: 'text' },
+                { name: 'outlier_datasets', type: 'text' },
+                { name: 'drift_datasets', type: 'text' },
+                { name: 'special_missing_codes', type: 'text' }
+            ]
+        },
+        'Test Code 11233': {
+            params: [
+                { name: 'handle_missing_values', type: 'text' },
+                { name: 'max_outlier', type: 'text' },
+                { name: 'correction', type: 'text' },
+                { name: 'outlier_datasets', type: 'text' },
+                { name: 'drift_datasets', type: 'text' },
+                { name: 'special_missing_codes', type: 'text' }
+            ]
+        }
+    }
+
     return (
-        <div style={{display: 'flex', flexDirection: 'column', fontSize: '12px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', fontSize: '12px', height: '77%', overflow: 'scroll'}}>
             <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
                 Select Signed-off Test Suite
                 <select name="page_size" id="page_size" style={{border: '1px solid black', borderRadius: '5px', margin: '4px 0px', padding: '8px 12px'}}>
@@ -23,6 +68,46 @@ export function Parameters(){
                     <option value="11216">Suite 11216</option>
                     <option value="11217">Suite 11217</option>
                 </select>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
+                Select Test Code
+                <div style={{display: 'flex', flexDirection: 'row', margin: '4px 0px', overflow: 'auto'}}>
+                    {   
+                        Object.keys(testCodes).map((testCode, idx) => {
+                            if( currentTestCode == testCode )
+                                return <div key={idx} style={{padding: '4px', border: '2px solid var(--wf-red)', borderRadius: '4px', margin: '4px', marginLeft: '0px'}} onClick={()=> setCurrentTestCode(testCode)}>
+                                    {testCode}
+                                </div>
+                            
+                            return <div key={idx} style={{padding: '4px', border: '1px solid grey', borderRadius: '4px', margin: '4px', marginLeft: '0px'}} onClick={()=> setCurrentTestCode(testCode)}>
+                                {testCode}
+                            </div>
+
+                        })
+                    }
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
+                Fill in the parameters
+                <div style={{display: 'grid', gridTemplateColumns: 'auto auto', gap: '4px', margin: '4px 0px'}}>
+                    {testCodes[currentTestCode].params.map((param, idx) => <input key={idx} placeholder={param.name} type={param.type} style={{ padding: '4px', border: '1px solid grey', borderRadius: '4px'}}/>)}
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', margin: '8px 4px'}}>
+                Criterion
+                <div style={{display: 'flex', flexDirection: 'row', margin: '4px 0px'}}>
+                    <input placeholder='Criteria Name' type='text' style={{ padding: '4px', border: '1px solid grey', borderRadius: '4px', margin: '4px', marginLeft: '0px'}}/>
+                    <select name="operator" id="operator" style={{border: '1px solid black', borderRadius: '5px', margin: '4px', padding: '8px 12px'}}>
+                        <option value="11213"> {'<'} </option>
+                        <option value="11214"> {'>'} </option>
+                        <option value="11215"> {'='} </option>
+                    </select>
+                    <input placeholder='Criteria Value' type='text' style={{ padding: '4px', border: '1px solid grey', borderRadius: '4px', margin: '4px'}}/>
+                </div>
+                <button style={{border: '1px solid grey', borderRadius: '4px', padding: '4px', width: '175px', justifyContent: 'center', alignItems: 'baseline'}}> 
+                    <AddIcon style={{ fontSize: '14px'}}/>
+                    Add New Criteria
+                </button>
             </div>
         </div>
     )
