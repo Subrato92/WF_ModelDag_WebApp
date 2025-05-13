c8867457d45735d55c51cdaf6fe5109768b738b7
SUBRATO MONDAL <subrato.mondal18@gmail.com>
2025-05-13 19:26:16 +0530

    Home Page Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import Image from "next/image";
 import { useRouter } from 'next/navigation';
+import ValOps from '@/resources/ValOps.svg'
+import Style from './Style.module.css';
 
 export default function Home() {
   const router = useRouter();
@@ -8,44 +10,23 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <span className="text-2xl">ValOps</span>
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by creating new {" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold" onClick={() => router.push('/pipeline')}>
-              Pipelines
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div style={{width: '100%', display: 'flex', flexDirection: 'row', height: '60vh', alignContent: 'baseline', justifyContent: 'center'}}>
+          <div style={{ flex: '0 1 50%' }}>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={ValOps}
+              alt="ValidationOperations"
+              width="400"
+              style={{translate: '10px 120px'}}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div style={{ flex: '1 1 50%' }}>
+            <Image
+              src='/pipelineSample.jpeg'
+              alt="DAG Pipeline"
+              width="800"
+              height="800"
+            />
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -64,11 +45,9 @@ export default function Home() {
           />
           Learn
         </a>
-        <a
+        <button
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => router.push('/pipeline')}
         >
           <Image
             aria-hidden
@@ -77,8 +56,8 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
-        </a>
+          Dashboard
+        </button>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -92,7 +71,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to ValOps.wellsfargo.net →
         </a>
       </footer>
     </div>
